c86a1363e4e5ec54504a953435cf9a0776ecdc2a
Ronghai <ronghai.wei@outlook.com>
2013-12-10 16:59:58 -0500

    add BasicCURDPanel

--- src/main/java/me/ronghai/sa/view/delegate/JTableBindingDelegate.java
+++ src/main/java/me/ronghai/sa/view/delegate/JTableBindingDelegate.java
@@ -12,3 +12,25 @@ package me.ronghai.sa.view.delegate;
 public interface JTableBindingDelegate  <E>{
    // JTableBinding getJTableBinding(AutoBinding.UpdateStrategy strategy, java.util.List<E> sourceList, javax.swing.JTable targetJTable, java.lang.String name);
 }
+/*
+
+   org.jdesktop.swingbinding.JTableBinding jTableBinding = org.jdesktop.swingbinding.SwingBindings.createJTableBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, customerList, dataTable);
+        org.jdesktop.swingbinding.JTableBinding.ColumnBinding columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${addressline2}"));
+        columnBinding.setColumnName("Addressline2");
+        columnBinding.setColumnClass(String.class);
+        columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${discountCode}"));
+        columnBinding.setColumnName("Discount Code");
+        columnBinding.setColumnClass(com.toy.anagrams.lib.DiscountCode.class);
+        columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${creditLimit}"));
+        columnBinding.setColumnName("Credit Limit");
+        columnBinding.setColumnClass(Integer.class);
+        columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${email}"));
+        columnBinding.setColumnName("Email");
+        columnBinding.setColumnClass(String.class);
+        columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${phone}"));
+        columnBinding.setColumnName("Phone");
+        columnBinding.setColumnClass(String.class);
+        bindingGroup.addBinding(jTableBinding);
+        jTableBinding.bind();
+
+*/
