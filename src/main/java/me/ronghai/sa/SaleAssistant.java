79a26a83b9ad7c18fc4a21efb3ce142c0e90a73a
Ronghai Wei <ronghai.wei@msn.com>
2013-12-11 01:27:11 -0500

    update mac os x interface

--- src/main/java/me/ronghai/sa/SaleAssistant.java
+++ src/main/java/me/ronghai/sa/SaleAssistant.java
@@ -6,6 +6,10 @@
 
 package me.ronghai.sa;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import me.ronghai.sa.bean.DataWrapperBean;
 import static me.ronghai.sa.core.dispatcher.SaleAssistansDispatcher.doDispatch;
 import me.ronghai.sa.view.frame.SaleAssistantFrame;
@@ -16,17 +20,31 @@ import me.ronghai.sa.view.action.callback.DispatcherCallBack;
  */
 
 public class SaleAssistant {
-    public static void main(String args[]){
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
+    private static String OS = System.getProperty("os.name").toLowerCase();
+    public static void main(String args[]){        
+        if ((OS.indexOf("mac") >= 0)) {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Test");
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                Logger.getLogger(SaleAssistant.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SaleAssistant.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } else {
+            try {
+                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+                }
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+                java.util.logging.Logger.getLogger(SaleAssistant.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
         }
+        
+        
+
 
         doDispatch("saleAssistantController.initView", null, null, null, new DispatcherCallBack(){
             @Override
