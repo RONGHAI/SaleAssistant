6c7c429bbca56cb460c92a205599df9fde68b633
Ronghai Wei <ronghai.wei@msn.com>
2014-04-02 23:11:58 -0400

    fix V4 sql

--- src/main/resources/db/migration/V4__changeworker2controller.sql
+++ src/main/resources/db/migration/V4__changeworker2controller.sql
@@ -2,5 +2,5 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-update navigations set worker = REPLACE(worker, "com.ecbeta.app.engine.worker", "com.ecbeta.app.engine.controller")
-update navigations set worker = REPLACE(worker, "Worker", "Controller") 
+update navigations set worker = REPLACE(worker, "com.ecbeta.app.engine.worker", "com.ecbeta.app.engine.controller");
+update navigations set worker = REPLACE(worker, "Worker", "Controller") ;
