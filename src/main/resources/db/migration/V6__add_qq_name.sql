41111787980350fd8d48d9fff120cac0f60247de
Weiwei Li <liweiwei_ny@yahoo.com>
2014-11-07 23:43:24 -0500

    add qqName to clients

--- src/main/resources/db/migration/V6__add_qq_name.sql
+++ src/main/resources/db/migration/V6__add_qq_name.sql
@@ -1,4 +1,4 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-ALTER TABLE `clients` ADD  `qqname` varchar(100);
+ALTER TABLE `clients` ADD  `qq_name` varchar(100) NOT NULL AFTER `qq`;
