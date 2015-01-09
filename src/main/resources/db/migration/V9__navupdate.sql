9771e3c1c13a695a9d3f19ddcfc1fa5a7f88aa83
RONGHAI <RONGHAI.WEI@OUTLOOK.COM>
2015-01-09 13:13:56 -0500

    udpate sql with new navigations

---
+++ src/main/resources/db/migration/V9__navupdate.sql
@@ -0,0 +1,12 @@
+
+
+
+INSERT INTO `navigations` ( `tier_1`, `tier_2`, `tier_3`, `tier_4`, `worker`, `label`, `order`, `disabled`) VALUES
+( 2, 2, 0, 0, 'com.ecbeta.app.engine.controller.SystemInformationController', '系统配置', 0, 0),
+( 2, 3, 0, 0, 'com.ecbeta.app.engine.controller.CurrencyController', '汇率配置', 0, 0),
+( 1, 3, 0, 0, 'com.ecbeta.app.engine.controller.AccountController', '账户设置', 0, 0),
+( 1, 4, 0, 0, 'com.ecbeta.app.engine.controller.CarrierController', '承运人设置', 0, 0),
+( 1, 6, 0, 0, 'com.ecbeta.app.engine.controller.MerchantController', '商户信息', 0, 0),
+( 1, 7, 0, 0, 'com.ecbeta.app.engine.controller.ProductController', '商品信息', 0, 0),
+( 1, 8, 0, 0, 'com.ecbeta.app.engine.controller.CategoryController', '商品类别', 0, 0)
+;
