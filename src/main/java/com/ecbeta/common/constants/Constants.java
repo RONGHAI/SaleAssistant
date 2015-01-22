463c51cb1efcef1a986a274d654fef8e72e39606
RONGHAI <RONGHAI.WEI@OUTLOOK.COM>
2015-01-22 17:46:30 -0500

    update Constants file

--- src/main/java/com/ecbeta/common/constants/Constants.java
+++ src/main/java/com/ecbeta/common/constants/Constants.java
@@ -19,4 +19,6 @@ public class Constants {
     public static final String PROGRESS_PAGE_POSTBACK="progress_page_postback"; 
     public static final String PANEL_INDEX = "panelIndex";
     public static final String DEFAULT_WORKER = "com.ecbeta.app.engine.controller.HomeController";
+    public static final String SAJS_PREFIX = "sale_assistant";
+    public static final String[] W2UI_CHANGES = new String[]{ "changes","changed"};
 }
