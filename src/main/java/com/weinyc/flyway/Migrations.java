7814e853374be5c14c62f9c3dca76dde7ce065da
RONGHAI <RONGHAI.WEI@OUTLOOK.COM>
2015-12-11 13:05:36 -0500

    flyway Migrations update with mysql and postgres support both

--- src/main/java/com/weinyc/flyway/Migrations.java
+++ src/main/java/com/weinyc/flyway/Migrations.java
@@ -1,12 +1,30 @@
 package com.weinyc.flyway;
+
+import java.net.URI;
 import org.flywaydb.core.Flyway;
 public class Migrations {
     public static void main(String[] args) throws Exception {
         Flyway flyway = new Flyway();
+        //ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
         flyway.setLocations("heroku.migration");
-        flyway.setDataSource(System.getenv("JDBC_DATABASE_URL"),
-                             System.getenv("JDBC_DATABASE_USERNAME"),
-                             System.getenv("JDBC_DATABASE_PASSWORD"));
+        String url;
+        String user;
+        String password;
+        if (System.getenv("DATABASE_URL") != null && System.getenv("DATABASE_URL").length() > 0) {
+            URI uri = new URI(System.getenv("DATABASE_URL"));
+            String[] split = uri.getUserInfo() == null ? new String[]{null, null} : uri.getUserInfo().split(":");
+            user = split[0];
+            password = 1 >= split.length ? null : split[1];
+            url = "jdbc:postgresql://" + uri.getHost() + (uri.getPort() >= 0 ? (":" + uri.getPort()) : "") + uri.getPath();
+            if (uri.getScheme().contains("mysql")) {
+                url = "jdbc:mysql://" + uri.getHost() + ":" + (uri.getPort() >= 0 ? (":" + uri.getPort()) : "") + uri.getPath() + (uri.getQuery() != null ? ("?" + uri.getQuery()) : "");
+            }
+        } else {
+            url = System.getenv("JDBC_DATABASE_URL");
+            user = System.getenv("JDBC_DATABASE_USERNAME");
+            password = System.getenv("JDBC_DATABASE_PASSWORD");
+        }
+        flyway.setDataSource(url, user, password);
         flyway.migrate();
     }
 }
