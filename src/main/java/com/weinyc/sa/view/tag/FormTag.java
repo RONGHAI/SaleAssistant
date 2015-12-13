31d268fa126ea2b6fe39b6aa14e35e1a490540d9
Weiwei Li <liweiwei_ny@yahoo.com>
2015-12-13 15:49:49 -0500

    update form action url

--- src/main/java/com/weinyc/sa/view/tag/FormTag.java
+++ src/main/java/com/weinyc/sa/view/tag/FormTag.java
@@ -57,7 +57,14 @@ public class FormTag  extends AbstractTag{
         
         StringBuilder sb = new StringBuilder();
         sb.append("<div id='form' > ").append("\n");
-        sb.append("<form class='formnomargin' name='").append(controller.getFORM_NAME()).append("'  id='").append(controller.getFORM_NAME()).append("'  action='").append(this.getContextPath()).append("/"+CORE_SERVLET+"' method='post'>").append("\n");
+        String action = this.getContextPath()+"/"+CORE_SERVLET+"/";
+        if(controller.getNavigationBean() != null){
+            action = controller.getNavigationBean().getUrl(getContextPath());
+        }
+        sb.append("<form class='formnomargin' name='").append(controller.getFORM_NAME())
+                .append("'  id='").append(controller.getFORM_NAME())
+                .append("'  action='").append(action).append("' method='post'>")
+                .append("\n");
         //sb.append("     <input type='hidden' name='" + REQUEST_WORKER + "' id='" + REQUEST_WORKER + "' value='").append(worker.getClass().getName()).append("' />").append("\n");
         //sb.append("     <input type='hidden' name='" + SRC_JSP + "' id='" + SRC_JSP + "' value='").append(worker.getJspGoto()).append("' />").append("\n");
         sb.append("     <input type='hidden' name='"+BTN_OPTION+"' id='"+BTN_OPTION+"'  value='' /> ").append("\n");
