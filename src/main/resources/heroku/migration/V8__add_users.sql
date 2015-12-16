71e6c41f9c6688da7aa866c392c2fcd427707d9d
RONGHAI <RONGHAI.WEI@OUTLOOK.COM>
2015-12-16 14:10:54 -0500

    fix V8 add users sql

--- src/main/resources/heroku/migration/V8__add_users.sql
+++ src/main/resources/heroku/migration/V8__add_users.sql
@@ -15,7 +15,6 @@ CREATE TABLE users
   note text,
   CONSTRAINT users_pkey PRIMARY KEY (id)
 );
-DROP SEQUENCE  IF EXISTS users_id_seq;
 CREATE SEQUENCE users_id_seq;
 ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('users_id_seq');
 ALTER TABLE users ALTER COLUMN id SET NOT NULL;
