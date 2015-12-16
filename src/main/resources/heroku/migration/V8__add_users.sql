e14bf4ef2ef106c48f13d25dfce0079d10ae7a10
RONGHAI <RONGHAI.WEI@OUTLOOK.COM>
2015-12-16 13:51:03 -0500

    fix V8 add users sql

---
+++ src/main/resources/heroku/migration/V8__add_users.sql
@@ -0,0 +1,24 @@
+
+
+
+
+CREATE TABLE users
+(
+  id serial NOT NULL,
+  name text NOT NULL,
+  email text NOT NULL,
+  password text NOT NULL,
+  secret_token character varying(20) NOT NULL DEFAULT ''::character varying,
+  disabled smallint NOT NULL DEFAULT (0)::smallint,
+  add_time timestamp without time zone,
+  update_time timestamp without time zone,
+  note text,
+  CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+
+CREATE SEQUENCE users_id_seq;
+ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('users_id_seq');
+ALTER TABLE users ALTER COLUMN id SET NOT NULL;
+ALTER SEQUENCE users_id_seq OWNED BY users.id;    -- 8.2 or later
+
+ 
