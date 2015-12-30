a79af20e37804806fec55b3fda874394ec901907
Weiwei Li <liweiwei_ny@yahoo.com>
2015-12-29 19:49:48 -0500

    update sql

--- src/main/resources/heroku/migration/V9__add_role_to_users.sql
+++ src/main/resources/heroku/migration/V9__add_role_to_users.sql
@@ -2,6 +2,6 @@
 -- ALTER TABLE users DROP COLUMN role;
 
 ALTER TABLE users ADD COLUMN role character varying(50);
-ALTER TABLE users ALTER COLUMN role SET NOT NULL;
+-- ALTER TABLE users ALTER COLUMN role SET NOT NULL;
 ALTER TABLE users ALTER COLUMN role SET DEFAULT ''::character varying;
 
